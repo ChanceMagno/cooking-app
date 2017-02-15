bf253a2cb134e755a57e8f8452ea9aaa1ffc7882
Chance Magno and Nathaniel Meyer <nathaniel@>
2017-02-15 15:05:18 -0800

    Add forms and bus logic for 6 conversions

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,53 @@
 var litersToGallons = function(liters) {
 	return liters * 0.264172
 };
-
-var liters = parseInt(prompt("How many liters do you want to convert to gallons?"));
-
-alert(liters + " liters equals " + litersToGallons(liters) + " gallons.");
-
 var gallonsToLiters = function(gallons) {
-	return (gallons / 0.264172).toFixed(2);
+	return gallons / 0.264172
+};
+var poundsToKilograms = function(lbs) {
+	return lbs * 0.453592
+};
+var kilogramsToPounds = function(kg) {
+	return kg / 0.453592
 };
 
-var gallons = parseInt(prompt("How many gallons do you want to convert to liters?"));
+var ouncesToMilliliters = function(ounces) {
+	return ounces * 29.5735;
+};
 
-alert(gallons + " gallons equals " + gallonsToLiters(gallons) + " liters.");
+var millilitersToOz = function(mL) {
+	return mL / 29.5735;
+};
+
+$(document).ready(function(){
+	$("form#ozToML").submit(function(event) {
+		var mL = ouncesToMilliliters($("input#ounces").val());
+		$("#mlFromOz").text(mL);
+		event.preventDefault();
+	});
+	$("form#mLtoOz").submit(function(event) {
+		var oz = millilitersToOz($("input#mL").val());
+		$("#ozFromML").text(oz);
+		event.preventDefault();
+	});
+	$("form#lbsToKg").submit(function(event) {
+		var kg = poundsToKilograms($("input#lbs").val());
+		$("#kgFromLbs").text(kg);
+		event.preventDefault();
+	});
+	$("form#kgToLbs").submit(function(event) {
+		var mL = kilogramsToPounds($("input#kg").val());
+		$("#lbsFromKg").text(mL);
+		event.preventDefault();
+	});
+	$("form#gallonsToLiters").submit(function(event) {
+		var kg = gallonsToLiters($("input#gallons").val());
+		$("#litersFromGallons").text(kg);
+		event.preventDefault();
+	});
+	$("form#litersToGallons").submit(function(event) {
+		var mL = litersToGallons($("input#liters").val());
+		$("#gallonsFromLiters").text(mL);
+		event.preventDefault();
+	});
+});
